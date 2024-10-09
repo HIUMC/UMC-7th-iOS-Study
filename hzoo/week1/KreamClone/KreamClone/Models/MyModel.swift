2659f931a38b36d04c2d95c81900799769bab166
Hyeon-Ju <lhjivy1024@gmail.com>
2024-10-09 15:44:12 +0900

    Feat: week2 업로드

---
+++ hzoo/week1/KreamClone/KreamClone/Models/MyModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyModel.swift
+//  KreamClone
+//
+//  Created by 이현주 on 10/7/24.
+//
+
+import Foundation
---
+++ hzoo/week1/KreamClone/KreamClone/Views/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  KreamClone
+//
+//  Created by 이현주 on 10/7/24.
+//
+
+import UIKit
+
+class MyPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
