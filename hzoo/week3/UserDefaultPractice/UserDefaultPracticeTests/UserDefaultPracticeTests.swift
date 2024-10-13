65cd469ce89c13adec3b77d4bdd6e50b5abb70f2
Hyeon-Ju <lhjivy1024@gmail.com>
2024-10-13 19:18:10 +0900

    Feat: week3 실습 업로드

---
+++ hzoo/week3/UserDefaultPractice/UserDefaultPracticeTests/UserDefaultPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultPracticeTests.swift
+//  UserDefaultPracticeTests
+//
+//  Created by 이현주 on 10/13/24.
+//
+
+import Testing
+@testable import UserDefaultPractice
+
+struct UserDefaultPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
