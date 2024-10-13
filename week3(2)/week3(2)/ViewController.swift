e535a0ba1dc43ccc21e3299477d286c1de980229
lalaurel <ajinpark0518@naver.com>
2024-10-14 03:55:44 +0900

    week3(2) 파일 커밋

---
+++ week3(2)/week3(2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3(2)
+//
+//  Created by 어진 on 10/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
