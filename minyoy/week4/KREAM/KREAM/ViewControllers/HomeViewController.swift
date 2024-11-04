2192c271c2ba10190c50010c6cb1399bb7b80412
minyoy <65756020+minyoy@users.noreply.github.com>
2024-11-05 01:18:38 +0900

    week5
    week5

--- minyoy/week4/KREAM/KREAM/ViewControllers/HomeViewController.swift
+++ minyoy/week4/KREAM/KREAM/ViewControllers/HomeViewController.swift
@@ -11,6 +11,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = homeView
+        self.navigationController?.isNavigationBarHidden = true
         setupAction()
         setupDelegate()
     }
