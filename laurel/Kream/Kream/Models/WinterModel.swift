57cb41d702b02196661a786bffd97fee58616344
lalaurel <ajinpark0518@naver.com>
2024-12-02 02:38:44 +0900

    refactor: model url로 수정

---
+++ laurel/Kream/Kream/Models/WinterModel.swift
@@ -0,0 +1,47 @@
+//
+//  WinterModel.swift
+//  Kream
+//
+//  Created by 어진 on 12/2/24.
+//
+
+import Foundation
+
+struct WinterModel {
+    let image: String
+}
+
+final class DummyWinterData {
+    static let items: [WinterModel] = [
+        WinterModel(
+            image: "https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/sw/2024/07/16/20240716513448.jpg"
+        ),
+        WinterModel(
+            image: "https://pimg.mk.co.kr/news/cms/202408/31/news-p.v1.20240831.a99cdc5c002d4bcc8532ae7f50d5d65e_R.jpg"
+        ),
+        WinterModel(
+            image: "https://file.mk.co.kr/meet/neds/2024/05/image_readtop_2024_387343_17167866015985898.jpg"
+        ),
+        WinterModel(
+            image: "https://pimg.mk.co.kr/news/cms/202408/31/news-p.v1.20240831.31cbd9a0232443558ee157fc355526be_R.jpg"
+        ),
+        WinterModel(
+            image: "https://pimg.mk.co.kr/news/cms/202408/31/news-p.v1.20240831.a99cdc5c002d4bcc8532ae7f50d5d65e_R.jpg"
+        ),
+        WinterModel(
+            image: "https://file.mk.co.kr/meet/neds/2024/05/image_readtop_2024_387343_17167866015985898.jpg"
+        ),
+        WinterModel(
+            image: "https://thumbnews.nateimg.co.kr/view610///news.nateimg.co.kr/orgImg/sw/2024/07/16/20240716513448.jpg"
+        ),
+        WinterModel(
+            image: "https://pimg.mk.co.kr/news/cms/202408/31/news-p.v1.20240831.a99cdc5c002d4bcc8532ae7f50d5d65e_R.jpg"
+        ),
+        WinterModel(
+            image: "https://file.mk.co.kr/meet/neds/2024/05/image_readtop_2024_387343_17167866015985898.jpg"
+        ),
+        WinterModel(
+            image: "https://img.sbs.co.kr/newsnet/etv/upload/2024/03/08/30000912325_1280.jpg"
+        )
+    ]
+}
